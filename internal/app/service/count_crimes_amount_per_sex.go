a2a828bbc65f6e4f8e2dc3bbf98c4a1f6906dd82
caiogmrocha <caioguilherme739@gmail.com>
2024-11-17 23:35:09 -0300

    refactor(#1): improve CountCrimesAmountPerSexService code

--- internal/app/service/count_crimes_amount_per_sex.go
+++ internal/app/service/count_crimes_amount_per_sex.go
@@ -8,21 +8,45 @@ import (
 
 type CountCrimesAmountPerSexService struct{}
 
+type CountCrimesAmountPerSexData struct {
+	Male      int `json:"male"`
+	Female    int `json:"female"`
+	Unknown   int `json:"unknown"`
+	NonBinary int `json:"non_binary"`
+}
+
+const (
+	outputKey = "crimes_amount_per_sex"
+)
+
 func (s *CountCrimesAmountPerSexService) Execute(record *entity.Record, output *sync.Map) {
-	if record.VictSex == "" {
-		return
-	}
+	var crimesAmountPerSexData *CountCrimesAmountPerSexData
 
-	if record.VictSex == "" {
-		record.VictSex = "U"
-	}
+	value, ok := output.Load(outputKey)
 
-	if value, ok := output.Load(record.VictSex); ok {
-		output.Store(record.VictSex, value.(int)+1)
-		return
+	if !ok {
+		crimesAmountPerSexData = &CountCrimesAmountPerSexData{
+			Male:      0,
+			Female:    0,
+			Unknown:   0,
+			NonBinary: 0,
+		}
 	} else {
-		output.Store(record.VictSex, 1)
+		crimesAmountPerSexData = value.(*CountCrimesAmountPerSexData)
 	}
+
+	switch record.VictSex {
+	case "M":
+		crimesAmountPerSexData.Male++
+	case "F":
+		crimesAmountPerSexData.Female++
+	case "X":
+		crimesAmountPerSexData.NonBinary++
+	default:
+		crimesAmountPerSexData.Unknown++
+	}
+
+	output.Store(outputKey, crimesAmountPerSexData)
 }
 
 func NewCountCrimesAmountPerSexService() *CountCrimesAmountPerSexService {
