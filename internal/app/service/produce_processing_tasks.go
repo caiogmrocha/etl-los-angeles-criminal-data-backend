69fba862f5ac4053ea370e0faf566b356f08d748
caiogmrocha <caioguilherme739@gmail.com>
2024-11-22 20:01:16 -0300

    feat(#6): ensure that the ProduceProcessingTasksService send task to process.crimes-amount-per-period topic

--- internal/app/service/produce_processing_tasks.go
+++ internal/app/service/produce_processing_tasks.go
@@ -48,6 +48,10 @@ func (s *ProduceProcessingTasksService) Execute(ctx context.Context, databasePat
 			QueueName:  PROCESS_CRIMES_AMOUNT_PER_AREA_QUEUE_NAME,
 			RoutingKey: PROCESS_CRIMES_DATA_ROUTING_KEY,
 		},
+		{
+			QueueName:  PROCESS_CRIMES_AMOUNT_PER_PERIOD_QUEUE_NAME,
+			RoutingKey: PROCESS_CRIMES_DATA_ROUTING_KEY,
+		},
 	}
 
 	for _, queue := range queuesToBind {
